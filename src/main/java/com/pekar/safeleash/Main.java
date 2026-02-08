f224f4f5f3d33d73e77d9414685399afab6a394e
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2026-02-08 18:19:48 +0100

    code cleanup

--- src/main/java/com/pekar/safeleash/Main.java
+++ src/main/java/com/pekar/safeleash/Main.java
@@ -1,6 +1,7 @@
 package com.pekar.safeleash;
 
 import com.mojang.logging.LogUtils;
+import com.pekar.safeleash.events.EventRegistry;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
@@ -19,29 +20,16 @@ public class Main
     // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    // The constructor for the mod class is the first code that is run when your mod is loaded.
-    // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
-    public Main(IEventBus modEventBus, ModContainer modContainer) {
-        // Register the commonSetup method for modloading
-        modEventBus.addListener(this::commonSetup);
-
-
-        // Register ourselves for server and other game events we are interested in.
-        // Note that this is necessary if and only if we want *this* class (SafeLeash) to respond directly to events.
-        // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
+    public Main(IEventBus modEventBus, ModContainer modContainer)
+    {
         NeoForge.EVENT_BUS.register(this);
-    }
-
-    private void commonSetup(FMLCommonSetupEvent event) {
-        // Some common setup code
-//        LOGGER.info("HELLO FROM COMMON SETUP");
-
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
+    public void onServerStarting(ServerStartingEvent event)
+    {
         // Do something when the server starts
-        LOGGER.info("HELLO from server starting");
+//        LOGGER.info("HELLO from server starting");
     }
 }
--- src/main/java/com/pekar/safeleash/MainClient.java
+++ src/main/java/com/pekar/safeleash/MainClient.java
@@ -1,6 +1,5 @@
 package com.pekar.safeleash;
 
-import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
